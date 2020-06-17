ae882d5f4d4301c98d0db07c40a63b9c9e9789a2
Stanislav Zemlyakov <redtom@yandex.ru>
2020-06-17 10:52:02 +0300

    [ADDED] implemenation + some test cases for input validation

--- src/main/kotlin/com/github/rcd27/springtestcase/validation/RegisterInputValidation.kt
+++ src/main/kotlin/com/github/rcd27/springtestcase/validation/RegisterInputValidation.kt
@@ -1,6 +1,10 @@
 package com.github.rcd27.springtestcase.validation
 
+import arrow.core.Invalid
 import arrow.core.NonEmptyList
+import arrow.core.None
+import arrow.core.Some
+import arrow.core.Valid
 import arrow.core.Validated
 import com.github.rcd27.springtestcase.rest.RegisterUserRequest
 import reactor.core.publisher.Mono
@@ -14,7 +18,32 @@ object RegisterInputValidation {
      */
     fun validate(input: RegisterUserRequest):
             Mono<Validated<NonEmptyList<RegisterUserValidationError>, RegisterUserRequest>> {
-        TODO("")
+
+        val errorList = mutableListOf<RegisterUserValidationError>()
+
+        val regex = "^([a-zA-Z0-9_\\-.]+)@(([a-zA-Z0-9_\\-]+)\\.){1,2}([a-zA-Z]{2,5})\$".toRegex()
+        if (input.email?.matches(regex) == false) {
+            errorList.add(RegisterUserValidationError("Invalid email"))
+        }
+
+        if (input.firstName.isNullOrBlank()) {
+            errorList.add(RegisterUserValidationError("First name is blank"))
+        }
+
+        if (input.lastName.isNullOrBlank()) {
+            errorList.add(RegisterUserValidationError("Last name name is blank"))
+        }
+
+        if (input.dateOfBirth.isNullOrBlank()) {
+            // TODO: add date parsing
+            errorList.add(RegisterUserValidationError("Please enter your birth date"))
+        }
+
+        // let information about cities be null
+        return when (val resultErrorList = NonEmptyList.fromList(errorList)) {
+            is Some -> Mono.just(Invalid(resultErrorList.t))
+            is None -> Mono.just(Valid(input))
+        }
     }
 }
 
