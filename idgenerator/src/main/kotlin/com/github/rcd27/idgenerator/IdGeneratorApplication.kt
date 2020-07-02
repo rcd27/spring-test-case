83cf2871199268b4905c455ea08f8bc614865fcc
Stanislav Zemlyakov <redtom@yandex.ru>
2020-07-02 13:57:05 +0300

    [ADDED] integration test for `IdGenerationRepository`

--- idgenerator/src/main/kotlin/com/github/rcd27/idgenerator/IdGeneratorApplication.kt
+++ idgenerator/src/main/kotlin/com/github/rcd27/idgenerator/IdGeneratorApplication.kt
@@ -1,10 +1,11 @@
 package com.github.rcd27.idgenerator
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class IdGeneratorApplication {
-    fun someLibraryMethod(): Boolean {
-        return true
-    }
+open class IdGeneratorApplication
+
+fun main() {
+    runApplication<IdGeneratorApplication>()
 }
