4132b88cf2e5915dace620180b48409013ec4e5f
Stanislav Zemlyakov <redtom@yandex.ru>
2020-07-08 11:19:19 +0300

    [REMOVED] useless annotation

--- api/src/test/kotlin/com/github/rcd27/api/verification/VerificationIntegrationTest.kt
+++ api/src/test/kotlin/com/github/rcd27/api/verification/VerificationIntegrationTest.kt
@@ -7,15 +7,12 @@ import com.ninjasquad.springmockk.SpykBean
 import io.mockk.every
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.core.publisher.Mono
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@RunWith(SpringRunner::class)
 class VerificationIntegrationTest(@Autowired private val webClient: WebTestClient) {
 
     @SpykBean
