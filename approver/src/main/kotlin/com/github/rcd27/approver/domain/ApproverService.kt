dc58e090bcab656dd3e8189077fb7cd59a91f4a0
Stanislav Zemlyakov <redtom@yandex.ru>
2020-07-14 10:42:20 +0300

    [FIXED] json serialization

--- approver/src/main/kotlin/com/github/rcd27/approver/domain/ApproverService.kt
+++ approver/src/main/kotlin/com/github/rcd27/approver/domain/ApproverService.kt
@@ -1,8 +1,10 @@
 package com.github.rcd27.approver.domain
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.rcd27.approver.dto.ApprovalRequest
 import com.github.rcd27.approver.dto.ApprovalResponse
 import com.github.rcd27.approver.dto.ApprovalStatus
+import com.github.rcd27.approver.dto.MailerRequest
 import org.springframework.amqp.core.MessageBuilder
 import org.springframework.amqp.core.MessagePropertiesBuilder
 import org.springframework.amqp.rabbit.annotation.EnableRabbit
@@ -14,38 +16,39 @@ import reactor.core.publisher.Mono
 @Service
 @EnableRabbit
 class ApproverService(
-    private val requestValidator: RequestValidator,
-    @Autowired private val rabbitTemplate: RabbitTemplate
+        private val requestValidator: RequestValidator,
+        @Autowired private val rabbitTemplate: RabbitTemplate
 ) {
 
     fun sendForApproval(input: ApprovalRequest): Mono<ApprovalResponse> {
         /** For simplicity - validation is mocked and every request is approved */
         return requestValidator.validate(input)
-            .map { validationResult: ValidationResult ->
-                when (validationResult) {
-                    is Valid -> {
+                .map { validationResult: ValidationResult ->
+                    when (validationResult) {
+                        is Valid -> {
 
-                        // FIXME: выглядит уёбищно, подумать как лучше
-                        val json = "{\"id\" : \"${input.verificationId}\",\n\"status\" : \"Approved\" }"
-                        val jsonMessage = MessageBuilder.withBody(json.toByteArray())
-                            .andProperties(
-                                MessagePropertiesBuilder.newInstance().setContentType("application/json")
-                                    .build()
-                            ).build()
-                        rabbitTemplate.send("mailerQueue", jsonMessage)
+                            val json = jacksonObjectMapper().writeValueAsBytes(
+                                    MailerRequest(input.verificationId, "Approved")
+                            )
+                            val jsonMessage = MessageBuilder.withBody(json)
+                                    .andProperties(
+                                            MessagePropertiesBuilder.newInstance().setContentType("application/json")
+                                                    .build()
+                                    ).build()
+                            rabbitTemplate.send("mailerQueue", jsonMessage)
 
-                        ApprovalResponse(
-                            input,
-                            ApprovalStatus.Approved
-                        )
+                            ApprovalResponse(
+                                    input,
+                                    ApprovalStatus.Approved
+                            )
+                        }
+
+                        is Invalid ->
+                            ApprovalResponse(
+                                    input,
+                                    ApprovalStatus.Denied
+                            )
                     }
-
-                    is Invalid ->
-                        ApprovalResponse(
-                            input,
-                            ApprovalStatus.Denied
-                        )
                 }
-            }
     }
 }
