3d04f92c45d37bd02e49238a847371feaf65b811
Stanislav Zemlyakov <redtom@yandex.ru>
2020-07-25 12:39:24 +0300

    [CHANGED] beans moved to @Configuration

---
+++ api/src/main/kotlin/com/github/rcd27/api/ApiConfiguration.kt
@@ -0,0 +1,12 @@
+package com.github.rcd27.api
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+open class ApiConfiguration {
+
+    @Bean
+    open fun provideWebClient(): WebClient = WebClient.create()
+}
