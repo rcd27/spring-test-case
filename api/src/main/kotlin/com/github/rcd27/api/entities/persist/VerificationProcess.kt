5f0f58ab6b68bf53a56a12101f35571523e9d10b
Stanislav Zemlyakov <redtom@yandex.ru>
2020-07-25 14:32:06 +0300

    [ADDED] test case

--- api/src/main/kotlin/com/github/rcd27/api/entities/persist/VerificationProcess.kt
+++ api/src/main/kotlin/com/github/rcd27/api/entities/persist/VerificationProcess.kt
@@ -1,10 +1,11 @@
 package com.github.rcd27.api.entities.persist
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 data class VerificationProcess(
-        val id: String,
+        @Id val id: String,
         val status: VerificationStatus
 ) {
     enum class VerificationStatus {
--- api/src/test/kotlin/com/github/rcd27/api/verification/VerificationIntegrationTest.kt
+++ api/src/test/kotlin/com/github/rcd27/api/verification/VerificationIntegrationTest.kt
@@ -5,6 +5,7 @@ import com.github.rcd27.api.approval.data.ApprovalRequest
 import com.github.rcd27.api.approval.data.ApprovalResponse
 import com.github.rcd27.api.approval.data.ApprovalStatus
 import com.github.rcd27.api.entities.dto.VerificationRequest
+import com.github.rcd27.api.entities.persist.VerificationProcess.VerificationStatus.*
 import com.github.rcd27.api.idgeneration.data.IdGenerationRepository
 import com.google.common.truth.Truth
 import com.ninjasquad.springmockk.SpykBean
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
 import reactor.core.publisher.Mono
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,12 +28,12 @@ class VerificationIntegrationTest(@Autowired private val webClient: WebTestClien
     lateinit var approvalRepository: ApprovalRepository
 
     private val validRequest = VerificationRequest(
-        "Stanislav",
-        "Zemlyakov",
-        "redtom@yandex.ru",
-        "01.08.1989",
-        "Innopolis",
-        "Saint-Petersburg"
+            "Stanislav",
+            "Zemlyakov",
+            "redtom@yandex.ru",
+            "01.08.1989",
+            "Innopolis",
+            "Saint-Petersburg"
     )
 
     @Suppress("ReactiveStreamsUnusedPublisher")
@@ -39,24 +41,22 @@ class VerificationIntegrationTest(@Autowired private val webClient: WebTestClien
     fun setUp() {
         every { idGenerationRepository.generateUniqueId() } returns Mono.just("very-unique-shit")
         every { approvalRepository.sendForApproval(any()) } returns Mono.just(
-            ApprovalResponse(
-                request = ApprovalRequest.fromVerificationRequest("very-unique-shit", validRequest),
-                status = ApprovalStatus.Approved
-            )
+                ApprovalResponse(
+                        request = ApprovalRequest.fromVerificationRequest("very-unique-shit", validRequest),
+                        status = ApprovalStatus.Approved
+                )
         )
     }
 
     @Test
-    fun `should send for verification and return its id`() {
-        // TODO: either check that verification is sent, or rename the test case
-
+    fun `should return id of a verification process`() {
         val idGivenForVerificationRequest = webClient.post()
-            .uri("/api/v1/verification/verify")
-            .body(Mono.just(validRequest), VerificationRequest::class.java)
-            .exchange()
-            .expectStatus().isOk
-            .expectBody(String::class.java)
-            .returnResult().responseBody
+                .uri("/api/v1/verification/verify")
+                .body(Mono.just(validRequest), VerificationRequest::class.java)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody(String::class.java)
+                .returnResult().responseBody
 
         Truth.assertThat(idGivenForVerificationRequest).isNotEmpty()
     }
@@ -64,27 +64,29 @@ class VerificationIntegrationTest(@Autowired private val webClient: WebTestClien
     @Test
     fun `should return bad request for verification process which doesn't exist`() {
         webClient.get()
-            .uri("/api/v1/verification/status/123")
-            .exchange()
-            .expectStatus().isBadRequest
+                .uri("/api/v1/verification/status/123")
+                .exchange()
+                .expectStatus().isBadRequest
     }
 
     @Test
     fun `should return status for previously created verification process`() {
         val idGivenForVerificationRequest = webClient.post()
-            .uri("/api/v1/verification/verify")
-            .body(Mono.just(validRequest), VerificationRequest::class.java)
-            .exchange()
-            .expectStatus().isOk
-            .expectBody(String::class.java)
-            .returnResult().responseBody
+                .uri("/api/v1/verification/verify")
+                .body(Mono.just(validRequest), VerificationRequest::class.java)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody(String::class.java)
+                .returnResult().responseBody
 
-        webClient.get()
-            .uri("/api/v1/verification/status/$idGivenForVerificationRequest")
-            .exchange()
-            .expectStatus().isOk
+        val body = webClient.get()
+                .uri("/api/v1/verification/status/$idGivenForVerificationRequest")
+                .exchange()
+                .expectStatus().isOk
+                .expectBody<String>()
+                .returnResult()
+                .responseBody
+
+        Truth.assertThat(body).isAnyOf(APPROVED.toString(), DECLINED.toString(), IN_PROGRESS.toString())
     }
-
-    // TODO: test when not passing {id} for verificationStatus
-    // TODO: test when other unhandled exception is thrown (if it can be thrown in that chain of a VerificationHandler)
 }
