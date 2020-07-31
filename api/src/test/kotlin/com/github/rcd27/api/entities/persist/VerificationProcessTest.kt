a291c3d40a7b2d752519ee969cb43243a5ade764
Stanislav Zemlyakov <redtom@yandex.ru>
2020-07-31 14:31:46 +0300

    [FIXED] data-mongo-db tests

--- api/src/test/kotlin/com/github/rcd27/api/entities/persist/VerificationProcessTest.kt
+++ api/src/test/kotlin/com/github/rcd27/api/entities/persist/VerificationProcessTest.kt
@@ -1,15 +1,12 @@
 package com.github.rcd27.api.entities.persist
 
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.test.context.junit4.SpringRunner
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
 
-@RunWith(SpringRunner::class)
 @DataMongoTest
 class VerificationProcessTest(@Autowired private val reactiveMongoTemplate: ReactiveMongoTemplate) {
 
@@ -19,12 +16,12 @@ class VerificationProcessTest(@Autowired private val reactiveMongoTemplate: Reac
         val savePublisher: Mono<VerificationProcess> = reactiveMongoTemplate.save(verificationProcess)
 
         StepVerifier.create(savePublisher)
-                .expectNextMatches { it.id.isNotEmpty() && it.status == VerificationProcess.VerificationStatus.IN_PROGRESS }
-                .verifyComplete()
+            .expectNextMatches { it.id.isNotEmpty() && it.status == VerificationProcess.VerificationStatus.IN_PROGRESS }
+            .verifyComplete()
 
         val findById = reactiveMongoTemplate.findById("any_uuid", VerificationProcess::class.java)
         StepVerifier.create(findById)
-                .expectNextCount(1)
-                .verifyComplete()
+            .expectNextCount(1)
+            .verifyComplete()
     }
 }
